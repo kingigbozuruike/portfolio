ab8ed98a24a18d36b69c4ec4ebc5c088ceba58ca
The Tech King <user1@Kings-MacBook-Air.local>
2024-05-08 01:18:35 -0500

    created react.js file, alongside imported it to the app.js file. new folders were created

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.css"
+import Header from './components/header/header'
+
+const app = () => {
+  return (
+    <>
+    <Header />
+    </>
+  )
+}
+
+export default app
